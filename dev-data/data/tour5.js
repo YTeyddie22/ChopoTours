183044eac65282ee64fa0aded6144b1f9b48f79c
Teyddie22 <teyddie@gmail.com>
2021-12-15 10:47:19 +0300

    init

---
+++ dev-data/data/tour5.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+const tour5 = {
+	id: 5,
+	name: 'The Sports Lover',
+	startLocation: 'California, USA',
+	nextStartDate: 'July 2021',
+	duration: 14,
+	maxGroupSize: 8,
+	difficulty: 'difficult',
+	avgRating: 4.7,
+	numReviews: 23,
+	regPrice: 2997,
+	shortDescription:
+		'Surfing, skating, parajumping, rock climbing and more, all in one tour',
+	longDescription: `If you don't like the objectives of it please achieve But the pain in the film is irure to condemn, in pleasure it wants to escape from the pain of being cillus that no one pays for. The pain itself is important, but I give it to the pain of some great pain. Ut enim ad minimi pardon, who ours exercise, any kind of labor except to take the objectives out of it to the advantage of the consequences. They are the exceptions the blinds yearn for, they don't see, they are the ones who abandon their responsibilities to the fault that is soothing the soul's hardships. What is our exercise, any school of employment except to take advantage of the objectives from it. But if the pain is to blame, in the pleasure it wants to be the clumsy pain that escapes from the pain that none can result in!`,
+};
