80c844d1e40d41592016f06618d7b7f198b16cda
Teyddie22 <teyddie@gmail.com>
2021-12-20 19:42:43 +0300

    introducing mongo

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
 
   console.log("Hello from the middleware");
+
   next();
 });
 
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const dotEnv = require("dotenv");
 
 dotEnv.config({
@@ -5,6 +6,20 @@ dotEnv.config({
 });
 
 const app = require("./app");
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log(`Error: ${err}`));
 
 const port = 3000 || process.env.PORT;
 
