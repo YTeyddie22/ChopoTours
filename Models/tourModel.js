e9b088ab83557d9478530cf1acd3cd6b1a6f90c9
Teyddie22 <teyddie@gmail.com>
2022-01-16 20:21:53 +0300

    creating a module that imports data from the file system

--- Models/tourModel.js
+++ Models/tourModel.js
@@ -6,6 +6,48 @@ const tourSchema = new mongoose.Schema({
     required: [true, "A tour must have a name"],
     unique: true,
   },
+  duration: {
+    type: Number,
+    required: [true, "A tour must have a duration"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "A tour must have a Group Size"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "A tour must have a difficulty"],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  priceDiscount: {
+    type: Number,
+  },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, "A tour must have a summary"],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "Tour must have a cover image"],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
   price: {
     type: Number,
     required: [true, "A tour must have a price"],
---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,61 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+
+const Tour = require("../../Models/tourModel");
+
+const dotEnv = require("dotenv");
+
+dotEnv.config({
+  path: "./config.env",
+});
+
+//* Reads the JSON FILE and parses to an object
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, "utf-8")
+);
+
+//* Database password
+const DB = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log(`Error: ${err}`));
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log("Database loaded");
+  } catch (err) {
+    console.log(err);
+  }
+
+  process.exit();
+};
+
+const deleteAllData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log("Database deleted");
+  } catch (err) {
+    console.log(err);
+  }
+
+  process.exit();
+};
+
+if (process.argv[2] === "--import") {
+  importData();
+} else if (process.argv[2] === "--delete") {
+  deleteAllData();
+}
+
+console.log(process.argv);
