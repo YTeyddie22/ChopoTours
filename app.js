99207cc0326f34f70d2c1576266266119c83e99e
Teyddie22 <teyddie@gmail.com>
2022-02-23 21:46:16 +0300

    installed debugger and handling undehandled routes

--- app.js
+++ app.js
@@ -7,16 +7,22 @@ const app = express();
 const toursRouter = require("./route/tourRoutes");
 const userRouter = require("./route/userRoutes");
 
-//! Middleware;
-app.use(express.json());
-
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
+
+  //! Middleware;
+  app.use(express.json());
+  app.use(express.static(`${__dirname}/public`));
 }
 
 app.use("/api/v1/tours", toursRouter);
 app.use("/api/v1/users", userRouter);
 
-//! Listening to the port;
+app.all("*", (req, res, next) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Can't find ${req.originalUrl} in this server`,
+  });
+});
 
 module.exports = app;
