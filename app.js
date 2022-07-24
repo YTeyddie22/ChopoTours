c12a6e4b53a4e313a8d774305a0b294917864929
Teddy Yongo <teyddie@gmail.com>
2022-07-24 07:39:42 +0300

    setting security headers

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
+//? Packages
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 
 const app = express();
 
+//? Routers
 const toursRouter = require("./route/tourRoutes");
 const userRouter = require("./route/userRoutes");
 
+//? Global Error
 const AppError = require("./utils/appError");
-
 const globalErrorController = require("./controllers/errorController");
 
+//! Set security Headers;
+
+app.use(helmet());
+
+//! Logging middleware
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
-
-  //! Middleware;
-
-  app.use(express.json());
-  // ESLINT error when we write  `S{__dirname/public}`
-  app.use(express.static(`${__dirname}/public`));
 }
 
 //* Prevents too many requests.
 const limiter = rateLimit({
-  max: 3,
+  max: 100,
   windowMs: 60 * 60 * 1000,
   message: "Too many requests from this IP. Try again after a while",
 });
@@ -32,8 +34,11 @@ const limiter = rateLimit({
 app.use("/api", limiter);
 //* Routing middleware
 
-app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true }));
+//! bodyParsing Middleware;
+
+app.use(express.json({ limit: "10kb" }));
+// ESLINT error when we write  `S{__dirname/public}`
+app.use(express.static(`${__dirname}/public`));
 
 app.use("/api/v1/tours", toursRouter);
 app.use("/api/v1/users", userRouter);
