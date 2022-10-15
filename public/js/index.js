3517328e7957ebac28de556f7597814f8fd3ca25
Teddy Yongo <teyddie@gmail.com>
2022-10-15 08:50:43 +0300

    update password settings

--- public/js/index.js
+++ public/js/index.js
@@ -13,6 +13,7 @@ const mapBox = document.getElementById("map");
 const loginForm = document.querySelector(".form--login");
 const logOutBtn = document.querySelector(".nav__el--logout");
 const userDataForm = document.querySelector(".form-user-data");
+const userPasswordForm = document.querySelector(".form-user-password");
 
 //! Check for mapbox;
 
@@ -46,8 +47,33 @@ if (userDataForm)
     const email = document.getElementById("email").value;
     const name = document.getElementById("name").value;
 
-    updateSettings(name, email);
+    updateSettings({ name, email }, "data");
   });
+
+/**
+ * ! Update the User Password;
+ */
+
+if (userPasswordForm)
+  userPasswordForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    document.querySelector(".btn--save-password ").textContent = "Updating...";
+    const currentPassword = document.getElementById("password-current").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("password-confirm").value;
+
+    await updateSettings(
+      { currentPassword, password, confirmPassword },
+      "password"
+    );
+    document.querySelector(".btn--save-password ").textContent =
+      "save password".toUpperCase();
+    document.getElementById("password-current").value = "";
+    document.getElementById("password").value = "";
+    document.getElementById("password-confirm").value = "";
+  });
+
 /**
  *
  * TODO ~ Sign Up;
