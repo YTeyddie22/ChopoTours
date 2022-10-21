2010537e351a220423bd8a01d3b961191f1a6f95
Teddy Yongo <teyddie@gmail.com>
2022-10-21 09:51:40 +0300

    building an email class

--- utils/email.js
+++ utils/email.js
@@ -1,4 +1,11 @@
+const pug = require("pug");
 const nodemailer = require("nodemailer");
+const htmlToText = require("html-to-text");
+
+/**
+ *! An email handler  (Simplified);
+ */
+/*
 
 const sendEmail = async (options) => {
   //*1 Create a transporter.
@@ -27,3 +34,73 @@ const sendEmail = async (options) => {
 };
 
 module.exports = sendEmail;
+
+*/
+
+/**
+ *
+ * ! Creating a new Email class (Complex);
+ */
+
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(" ")[0];
+    this.url = url;
+    this.from = `Teddy Yongo <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === "production") {
+      /**
+       * TODO
+       * We will use SendGrid
+       */
+      return 1;
+    }
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      PORT: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  async send(template, subject) {
+    /**
+     * Render HTML based on PUG template;
+     * Define email options;
+     *
+     */
+    //* 1 Render HTML based on PUG template;
+
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      }
+    );
+
+    //* 2 Define email options;
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html),
+    };
+
+    //* 3 Create a transport and send email;
+
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  //? This is the first email;
+  async sendWelcome() {
+    await this.send("welcome", "Welcome to the ChopoTours Family!");
+  }
+};
